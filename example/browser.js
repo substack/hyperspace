011f9616dad8aa7199b5f3f0f0350933f2a09e70
James Halliday <mail@substack.net>
2013-05-23 01:23:37 -0700

    use split

--- example/browser.js
+++ example/browser.js
@@ -1,4 +1,5 @@
 var through = require('through');
+var split = require('split');
 var domify = require('domify');
 var render = require('./render');
 
@@ -6,7 +7,7 @@ var shoe = require('shoe');
 var stream = shoe('/sock');
 
 var rows = document.querySelector('#rows');
-stream.pipe(render()).pipe(through(function (html) {
+stream.pipe(split().pipe(render()).pipe(through(function (html) {
     var elems = domify(html);
     for (var i = 0; i < elems.length; i++) {
         rows.appendChild(elems[i]);
