9d49455d92744dad1206de8aa86327ef95c89e22
James Halliday <mail@substack.net>
2013-07-06 22:59:43 -0700

    convert to &#NNN; codes to avoid unicode troubles

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ module.exports = function (html, cb) {
         }
         else row = line;
         var res = cb(row);
-        if (res) this.queue(hyperglue(html, res).outerHTML);
+        if (res) this.queue(encode(hyperglue(html, res).outerHTML));
     });
 };
+
+function encode (s) {
+    var res = '';
+    for (var i = 0; i < s.length; i++) {
+        var c = s.charCodeAt(i);
+        if (c >= 128) {
+            res += '&#' + c + ';';
+        }
+        else res += s.charAt(i);
+    }
+    return res;
+}
