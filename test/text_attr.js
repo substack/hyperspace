c90edf5e1df3414c369aff1ed38d4a982ecf640f
James Halliday <mail@substack.net>
2014-09-27 03:56:03 +0300

    failing text+attr test

---
+++ test/text_attr.js
@@ -0,0 +1,48 @@
+var hyperspace = require('../');
+var test = require('tape');
+var concat = require('concat-stream');
+
+var html = '<div class="row">'
+    + '<div class="who"><a></a></div>'
+    + '<div class="message"></div>'
+    + '</div>'
+;
+
+test('simple obj', function (t) {
+    t.plan(1);
+    
+    var hs = hyperspace(html, function (row) {
+        return {
+            '.who': {
+                _text: row.who,
+                href: '/users/' + row.who
+            },
+            '.message': row.message
+        };
+    });
+    hs.pipe(concat(function (body) {
+        t.equal(fix(body.toString('utf8')), [
+            '<div class="row">'
+            + '<div class="who"><a href="/users/robot">robot</a></div>'
+            + '<div class="message">beep boop</div>'
+            + '</div>',
+            '<div class="row">'
+            + '<div class="who"><a href="/users/t-rex">t-rex</a></div>'
+            + '<div class="message">rawr!</div>'
+            + '</div>',
+            '<div class="row">'
+            + '<div class="who"><a href="/users/mouse">mouse</a></div>'
+            + '<div class="message">&#60;squeak&#62;</div>'
+            + '</div>'
+        ].join(''));
+    }));
+    
+    hs.write({ who: 'robot', message: 'beep boop' });
+    hs.write({ who: 't-rex', message: 'rawr!' });
+    hs.write({ who: 'mouse', message: '<squeak>' });
+    hs.end();
+});
+
+function fix (s) {
+    return s.replace(/&lt;/g, '&#60;').replace(/&gt;/, '&#62;');
+}
